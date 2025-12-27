ed8d4f5fbb969d0de314d76b7e570be1db211dad
daochengjack <153249514+daochengjack@users.noreply.github.com>
2025-12-27 09:30:25 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: 'Welcome to our company blog', url: 'https://blog.qdceramic.com' },
+    { title: 'Welcome to our company website', url: 'https://qdceramic.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,19 +18,19 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: 'Alumina Ceramic',
+  HEO_HERO_TITLE_2: 'Zirconia Ceramic',
+  HEO_HERO_TITLE_3: 'BLOG.QDCERAMIC.COM',
+  HEO_HERO_TITLE_4: 'Corundum Mullite',
+  HEO_HERO_TITLE_5: 'Steatite',
+  HEO_HERO_TITLE_LINK: 'https://qdceramic.com',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛',
+  HEO_HERO_COVER_TITLE: 'Browsing',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: 'Alumina Ceramic', url: '/tag/Alumina Ceramic' },
+  HEO_HERO_CATEGORY_2: { title: 'Zirconia Ceramic', url: '/tag/Zirconia Ceramic' },
+  HEO_HERO_CATEGORY_3: { title: 'Other Ceramics', url: '/tag/Other Ceramics' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -51,19 +51,19 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://blog.qdceramic.com',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  HEO_INFO_CARD_URL3: 'https://qdceramic.com',
+  HEO_INFO_CARD_TEXT3: 'Learn More',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
     {
-      title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
+      title_1: 'Fused Magnesia Zirconia Nozzle',
+      img_1: 'https://i.111666.best/image/Su35GgHlN9V5zV6ROIc6lJ.png',
       color_1: '#989bf8',
-      title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
+      title_2: 'Alumina Ceramic shaped parts',
+      img_2: 'https://i.111666.best/image/OlTWMJhsHy4xjBUwqnELgD.JPG',
       color_2: '#ffffff'
     },
     {
